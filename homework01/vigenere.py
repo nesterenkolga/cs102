104d9148b131db47fff42a01f9c1ba92e40df748
nesterenkolga <chxllbr@yandex.ru>
2023-03-31 22:36:04 +0300

    again

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,5 @@
+from string import ascii_uppercase, ascii_lowercase
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,8 +11,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    alf_apper = ascii_uppercase + ascii_uppercase
+    alf_lower = ascii_lowercase + ascii_lowercase
+    keyword_full = ""
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(0, len(plaintext)):
+        index = i % len(keyword)
+        keyword_full += keyword[index]
+    for i in range(0, len(plaintext)):
+        if plaintext[i] in alf_apper:
+            index = alf_apper.find(plaintext[i]) + alf_apper.find(keyword_full[i])
+            ciphertext += alf_apper[index]
+        elif plaintext[i] in alf_lower:
+            index = alf_lower.find(plaintext[i]) + alf_lower.find(keyword_full[i])
+            ciphertext += alf_lower[index]
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -25,6 +41,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+    alf_apper = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alf_lower = alf_apper.lower()
+    keyword_full = ""
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(0, len(ciphertext)):
+        index = i % len(keyword)
+        keyword_full += keyword[index]
+    for i in range(0, len(ciphertext)):
+        if ciphertext[i] in alf_apper:
+            index = alf_apper.find(ciphertext[i]) - alf_apper.find(keyword_full[i])
+            plaintext += alf_apper[index]
+        elif ciphertext[i] in alf_lower:
+            index = alf_lower.find(ciphertext[i]) - alf_lower.find(keyword_full[i])
+            plaintext += alf_lower[index]
+        else:
+            plaintext += ciphertext[i]
     return plaintext
