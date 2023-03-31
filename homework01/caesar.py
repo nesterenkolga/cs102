2be4c6eb9c4284ece214700a34259abbf0457f62
nesterenkolga <chxllbr@yandex.ru>
2023-03-31 23:05:55 +0300

    fix cs102

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -69,4 +69,6 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+
 print(decrypt_caesar(encrypt_caesar("python")))
