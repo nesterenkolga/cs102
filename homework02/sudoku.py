607a94011c5589996d049ec0b243d56977cc819d
nesterenkolga <chxllbr@yandex.ru>
2023-04-04 14:29:14 +0300

    again

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -3,8 +3,6 @@ import typing as tp
 import random
 from pyparsing import empty
 
-#T = tp.TypeVar("T")
-
 def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
     """ Прочитать Судоку из указанного файла """
     path = pathlib.Path(path)
